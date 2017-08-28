9a12c347135cd7b320aa4b90f0f11ffe2bb6f92f
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-08-27 22:23:45 -0400

    update

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var exps = express();
 
+const ApiAiApp = require('actions-on-google').ApiAiApp;
+
 exps.use(bodyParser.json());
 
+// API.AI actions
+const WELCOME_ACTION = 'input.welcome';
+
 exps.post('/hook', function(request, response) {
-  response.json({
-    "speech": "Hello from hook",
-    "displayText": "Hello from hook",
-    "data": {},
-    "contextOut": [
-      {"name":"get-address", "lifespan":1, "parameters":{"address_invocation":"address"}}
-    ],
-    "source": "Mr Sukha"
-  });
+  const app = new ApiAiApp({request, response});
+  function greetUser (app) {
+    app.tell("Hello World!");
+  }
+
+  let actionMap = new Map();
+  actionMap.set(WELCOME_ACTION, greetUser);
+
+  app.handleRequest(actionMap);
 });
 
 exps.listen((process.env.PORT || 7001), function() {
